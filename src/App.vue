e051ef23fed3ecb68d69c83d82a52b8eee9a8423
ravi4j <ravi4j@icloud.com>
2020-01-01 19:12:12 -0600

    Added Bootstrap and externalized the items

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div id="app">
-    <RoutingMap origin ="INDEPENDENCE,JACKSON,MO,64058,USA" destination="1701 BROAD ST,STORY CITY,IA,50248 , USA" />
+    <b-card title="Card Title" no-body>
+      <b-card-header header-tag="nav">
+        <b-nav card-header tabs>
+          <b-nav-item
+            v-for="item in items"
+            v-bind:key="item.id"
+            :active="item.state"
+            @click="navigateTo(item.id)"
+          >{{item.label}}</b-nav-item>
+        </b-nav>
+      </b-card-header>
+      <b-card-body class="text-center">
+        <b-card-text>With supporting text below as a natural lead-in to additional content.</b-card-text>
+        <RoutingMap v-if="items && items[0].state" 
+          origin="INDEPENDENCE,JACKSON,MO,64058,USA"
+          destination="1701 BROAD ST,STORY CITY,IA,50248 , USA"
+        />
+        <div v-if="items && items[1].state" >I am {{items[1].label}}</div>
+        <div v-if="items && items[2].state" >I am {{items[2].label}}</div>
+      </b-card-body>
+    </b-card>
   </div>
 </template>
 
@@ -12,21 +32,49 @@ export default {
   components: {
     RoutingMap
   },
-  data(){
+  data() {
     return {
-    data: null
-    }
+      waitFor: null,
+      data: null,
+      items: null
+    };
   },
-  async created(){
-      await axios
+  async created() {
+    let createResolve = null;
+    this.waitFor = new Promise(function(resolve) {
+      createResolve = resolve;
+    });
+    await axios
       .get(`/static/data.json`)
       .then(response => {
         // JSON responses are automatically parsed.
         this.data = response.data;
+        this.items = this.data.items;
       })
       .catch(e => {
         this.errors.push(e);
       });
+    createResolve(this.items);
+    console.log("created return with.." + JSON.stringify(this.items));
+  },
+  async mounted() {
+   await  this.waitFor.then(r => {
+      console.log("mounted waiting for items .." + JSON.stringify(r));
+    });
+    console.log("mounted return with" + JSON.stringify(this.items));
+  },
+  methods: {
+    navigateTo(item) {
+      console.log("Navigate to item..." + item);
+      for (var i = 0; i < this.items.length; i++) {
+        console.log("Processing..." + JSON.stringify(this.items[i]));
+        if (this.items[i].id == item) {
+          this.items[i].state = true;
+        } else {
+          this.items[i].state = false;
+        }
+      }
+    }
   }
 };
 </script>
